dd069a7a5147e53897d15377e47e764858ddd1f7
amandazzoc <amandaol7b@gmail.com>
2024-05-11 16:39:36 -0300

    Efeito reveal

---
+++ js/reveal.js
@@ -0,0 +1,151 @@
+window.revelar = ScrollReveal({
+    reset: true
+})
+
+// TOPO DO SITE
+revelar.reveal('.efeito-txt-topo',
+    {
+        duration: 2000,
+        distance: '90px'
+    })
+revelar.reveal('.efeito-img-topo',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 500
+    })
+
+// CARDS
+revelar.reveal('.efeito-card-1',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 500
+    })
+revelar.reveal('.efeito-card-2',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 1000
+    })
+revelar.reveal('.efeito-card-3',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 1500
+    })
+revelar.reveal('.efeito-card-4',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 2000
+    })
+
+// INTEGRANTES
+revelar.reveal('.efeito-text-integrantes',
+    {
+        duration: 2000,
+        distance: '90px',
+
+    })
+revelar.reveal('.efeito-integrantes1',
+    {
+        duration: 2000,
+        distance: '90px',
+        origin: 'right',
+        delay: 0
+    })
+revelar.reveal('.efeito-integrantes2',
+    {
+        duration: 2000,
+        distance: '90px',
+        origin: 'right',
+        delay: 500,
+    })
+revelar.reveal('.efeito-integrantes3',
+    {
+        duration: 2000,
+        distance: '90px',
+        origin: 'right',
+        delay: 1000
+    })
+revelar.reveal('.efeito-integrantes4',
+    {
+        duration: 2000,
+        distance: '90px',
+        origin: 'right',
+        delay: 1500
+    })
+
+// MOBO
+revelar.reveal('.efeito-mockup',
+    {
+        duration: 2000,
+        distance: '90px',
+        origin: 'left',
+    })
+revelar.reveal('.efeito-txt-mobo1',
+    {
+        duration: 2000,
+        distance: '90px',
+        origin: 'rigth',
+    })
+revelar.reveal('.efeito-fundo-mobo',
+    {
+        duration: 2000,
+        distance: '90px',
+    })
+
+// ITENS 
+revelar.reveal('.efeito-img-braco',
+    {
+        duration: 2000,
+        distance: '90px',
+        origin: 'right',
+    })
+revelar.reveal('.efeito-lista1',
+    {
+        duration: 2000,
+        distance: '90px',
+        origin: 'left',
+    })
+revelar.reveal('.efeito-lista2',
+    {
+        duration: 2000,
+        distance: '90px',
+        origin: 'left',
+        delay: 300
+    })
+revelar.reveal('.efeito-lista3',
+    {
+        duration: 2000,
+        distance: '90px',
+        origin: 'left',
+        delay: 600
+    })
+revelar.reveal('.efeito-lista4',
+    {
+        duration: 2000,
+        distance: '90px',
+        origin: 'left',
+        delay: 900
+    })
+
+// MOBO CONTENT
+revelar.reveal('.efeito-fundo-mobo2',
+    {
+        duration: 2000,
+        distance: '90px',
+        origin: 'left',
+    })
+revelar.reveal('.efeito-img-logo',
+    {
+        duration: 2000,
+        distance: '90px',
+    })
+revelar.reveal('.efeito-txt-mobo2',
+    {
+        duration: 2000,
+        distance: '90px',
+        origin: 'rigth',
+    }) 
