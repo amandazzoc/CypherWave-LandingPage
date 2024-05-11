a278856af3c8c217fbf74f201b3f267686df4608
amandazzoc <amandaol7b@gmail.com>
2024-05-11 18:21:47 -0300

    nav-bar responsiva

---
+++ js/nav-bar.js
@@ -0,0 +1,20 @@
+// Seleciona o elemento com a classe "hamburger" e o elemento com a classe "nav"
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".nav");
+
+// Adiciona um event listener para o clique no elemento "hamburger"
+hamburger.addEventListener("click", () => {
+  // Adiciona ou remove a classe "active" do elemento "nav" quando o "hamburger" é clicado
+  nav.classList.toggle("active");
+});
+
+// Seleciona todos os links dentro da lista com a classe "nav-list"
+const navLinks = document.querySelectorAll(".nav-list a");
+
+// Para cada link, adiciona um event listener para o clique
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    // Remove a classe "active" do elemento "nav" quando um link é clicado
+    nav.classList.remove("active");
+  });
+});
--- js/reveal.js
+++ js/reveal.js
@@ -52,28 +52,24 @@ revelar.reveal('.efeito-integrantes1',
     {
         duration: 2000,
         distance: '90px',
-        origin: 'right',
         delay: 0
     })
 revelar.reveal('.efeito-integrantes2',
     {
         duration: 2000,
         distance: '90px',
-        origin: 'right',
         delay: 500,
     })
 revelar.reveal('.efeito-integrantes3',
     {
         duration: 2000,
         distance: '90px',
-        origin: 'right',
         delay: 1000
     })
 revelar.reveal('.efeito-integrantes4',
     {
         duration: 2000,
         distance: '90px',
-        origin: 'right',
         delay: 1500
     })
 
@@ -82,13 +78,11 @@ revelar.reveal('.efeito-mockup',
     {
         duration: 2000,
         distance: '90px',
-        origin: 'left',
     })
 revelar.reveal('.efeito-txt-mobo1',
     {
         duration: 2000,
         distance: '90px',
-        origin: 'rigth',
     })
 revelar.reveal('.efeito-fundo-mobo',
     {
@@ -101,33 +95,28 @@ revelar.reveal('.efeito-img-braco',
     {
         duration: 2000,
         distance: '90px',
-        origin: 'right',
     })
 revelar.reveal('.efeito-lista1',
     {
         duration: 2000,
         distance: '90px',
-        origin: 'left',
     })
 revelar.reveal('.efeito-lista2',
     {
         duration: 2000,
         distance: '90px',
-        origin: 'left',
         delay: 300
     })
 revelar.reveal('.efeito-lista3',
     {
         duration: 2000,
         distance: '90px',
-        origin: 'left',
         delay: 600
     })
 revelar.reveal('.efeito-lista4',
     {
         duration: 2000,
         distance: '90px',
-        origin: 'left',
         delay: 900
     })
 
@@ -136,7 +125,6 @@ revelar.reveal('.efeito-fundo-mobo2',
     {
         duration: 2000,
         distance: '90px',
-        origin: 'left',
     })
 revelar.reveal('.efeito-img-logo',
     {
@@ -147,5 +135,4 @@ revelar.reveal('.efeito-txt-mobo2',
     {
         duration: 2000,
         distance: '90px',
-        origin: 'rigth',
     }) 
